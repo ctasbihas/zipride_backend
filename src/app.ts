a121116132a19dd2c23a4728c4e74b1eb40e040b
ctasbihas <ctasbihas@gmail.com>
2025-10-25 19:18:11 +0600

    Refactor CORS configuration: replace environment variables with hardcoded allowed origins for improved clarity

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Request, Response } from "express";
-import { env } from "./app/config/env";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import { indexRoutes } from "./app/routes";
 
@@ -9,10 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = [
-	env.DEVELOPMENT_FRONTEND_URL,
-	env.PRODUCTION_FRONTEND_URL,
-];
+const allowedOrigins = ["http://localhost:3000", "https://zipride.vercel.app"];
 
 app.use(
 	cors({
